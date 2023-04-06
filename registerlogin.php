97e526067422d0c09d51e5e7580fac1b0d594c74
aliamuhd8 <107701670+aliamuhd8@users.noreply.github.com>
2023-04-06 18:54:10 +0800

    Update registerlogin.php

--- registerlogin.php
+++ registerlogin.php
@@ -16,7 +16,7 @@
       <input type="text" id="username" name="username" required pattern="[a-zA-Z0-9]{4,16}" title="Please enter a username between 4 and 16 characters that contains only letters and numbers.">
 
       <label for="password">Password:</label>
-      <input type="password" id="password" name="password" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}" title="Please enter a password that is at least 8 characters long and contains at least one uppercase letter, one lowercase letter, one number, and one special character.">
+      <input type="password" id="password" name="password" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@_-#$%^&*]).{8,}" title="Please enter a password that is at least 8 characters long and contains at least one uppercase letter, one lowercase letter, one number, and one special character.">
 
       <input type="submit" value="Login">
       <p>Already have an account? <a href="authpage.php">Login here.</a></p>
